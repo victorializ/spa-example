10ac4499f9b9a9323df4aa07dd1f88bdeb554a7a
victorializ <aflvictoriai@gmail.com>
2020-10-08 22:43:04 +0300

    routing poluchshe

--- routrer.js
+++ routrer.js
@@ -1,13 +1,37 @@
-let currentPage;
+const pages = new Map();
+const root = document.getElementById('root');
 
-function route(pageName) {
-    currentPage && document.getElementById(currentPage).setAttribute("style", "display: none");
-    pageName && document.getElementById(pageName).setAttribute("style", "display: visible");
-    currentPage = pageName;
+function loadPage(pathName) {
+    console.log('fetch')
+    return fetch(`./components/${pathName}/index.html`)
+        .then(response => response.text())
+        .then(html => {
+            pages.set(pathName, html);
+            return html;
+        });
+}
+
+function updateRoot(html) {
+    root.innerHTML = html;
+    return html;
+}
+
+function updateUrl(pathName) {
+    window.history.pushState({}, pathName,
+        `${window.location.origin}/${pathName}`
+    );
+}
+
+function route(pathName) {
+    updateUrl(pathName);
+    updateRoot(pages.get(pathName)) 
+        || loadPage(pathName).then(updateRoot);
 }
 
 function onLoad() {
-    route('home');
+    const homePathName = 'home';
+    const pathName = window.location.pathname.replace('/', '') || homePathName;
+    route(pathName);
 }
 
 window.onload = onLoad();
